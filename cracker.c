2e30905469cf1916ba91dcc0b0785ddc6eb43de5
Arsal <shaikhar@grinnell.edu>
2025-12-05 15:46:09 -0600

    Implemented simple enigma.c

---
+++ cracker.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+// establish a random setting
+// run worker threads on various rotor orders
+// Choose best five rotor orders (simple version can choose the best)
+// Fix rotor order and run worker threads on various rotor configurations
+// Choose best five rotor configurations (simple version can choose the best)
+// Fix rotor configurations
+// Output the resulting plain text on the best settings
+
+// Need to consitent
+// Encrypt()
+// type Settings
+
+// Need to implement worker to find all permutations of length 3 from the 5 rotors
+// Take inspiration from password cracker lab
+
+void* worker (void* args) {
+    return NULL;
+
+}
+
+
+int main(int argc, char* argv[]) {
+
+
+    return 0;
+}
--- engima.c
+++ engima.c
@@ -16,6 +16,7 @@ char* ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // Perhaps convert to hashmap
 
 // The rotors are ordered in terms of rotation frequency from fastest to slowest, right to left.
 typedef struct settings {
+    // Increasing index ordered slow to fast rotor
     int rotor_order[3];
     int rotor_offset[3];
 } 
@@ -32,13 +33,17 @@ char map(char current_char, char* current_rotor) {
     return '0';
 }
 
+/**
+ * Encrypts each character of a given string using thr provided initial settings
+ * Stores the encrypted text back into `text`. 
+ * `text` must be dynamically allocated memory
+ */
 char* encrypt(char* text, settings_t* setting) {
 
-    
     // Get length of message
     int text_length = strlen(text);
 
-    int* placed_rotors = setting->rotor_order; 
+    int* rotor_order = setting->rotor_order; 
     int* rotor_offset = setting->rotor_offset;
 
     // Encrypt each character
@@ -46,31 +51,52 @@ char* encrypt(char* text, settings_t* setting) {
     for (int c = 0; c < text_length; c++) {
         current_char = toupper(text[c]);
 
-        // Increment the rotor
+
+        // Pass through each rotor (slowest to fasted)
+        for (int r = 2; r >= 0; r--) {
+            
+            // Pass through rotor
+            char* current_rotor = ROTORS[rotor_order[r]];
+            
+            // Account for starting offset
+            current_char += rotor_offset[r];
+
+            // Pass character through the rotor
+            current_char = map(current_char, current_rotor);
+        }
+
+        // Increment the first rotor
         rotor_offset[0]++;
         if (rotor_offset[0] >= 26) {
             rotor_offset[0] = 0;
             rotor_offset[1]++;
         }
+        // If full rotation increment the second rotor
         if (rotor_offset[1] >= 26) {
             rotor_offset[1] = 0;
             rotor_offset[2]++;
         }
-        if (rotor_offset[2] >= 26) {
-            rotor_offset[0] = 0;
-        }
 
-        // Pass through each rotor
-        for (int rotor = 0; rotor < 1; rotor++) {
+        // If full roation then reset third rotor
+        rotor_offset[2] %= 26;
+
+        // Chose to ignore reflector because it dosen't make sense without a plugboard
+        // // Pass back through each rotor (fastest to slowest)
+        // for (int r = 0; r >= 2; r++) {
             
-            // Pass through rotor
-            char* current_rotor = ROTORS[placed_rotors[rotor]];
-            current_char += rotor_offset[rotor];
-            current_char = map(current_char, current_rotor);
+        //     // Pass through rotor
+        //     char* current_rotor = ROTORS[placed_rotors[r]];
 
-        }
+        //     // Account for starting offset
+        //     current_char += rotor_offset[r];
+
+        //     // Pass character through the rotor
+        //     current_char = map(current_char, current_rotor);
+
+        // }
 
 
+        // Store back into 
         text[c] = current_char;
     }
 
@@ -91,7 +117,7 @@ int main(int argc, char* argv[]) {
     // Define rotor settings
     settings_t* setting = malloc(sizeof(settings_t));
     for (int i = 0; i < 3; i++) {
-        setting->rotor_order[i] = i;
+        setting->rotor_order[i] = 2 - i;
         setting->rotor_offset[i] = 0;
     }
 
@@ -100,6 +126,6 @@ int main(int argc, char* argv[]) {
 
     printf("%s\n", cipher_text);
 
-
+    free(setting);
     free(text);
 }
