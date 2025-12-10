56e84d0fd0d3df3a195119c1a48c0692161a3039
Arsal <shaikhar@grinnell.edu>
2025-12-10 15:28:45 -0600

    Implementing cracker.c

--- cracker.c
+++ cracker.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <ctype.h>
 
+#include "enigma.c"
+
 // establish a random setting
 // run worker threads on various rotor orders
 // Choose best five rotor orders (simple version can choose the best)
@@ -18,14 +20,99 @@
 // Need to implement worker to find all permutations of length 3 from the 5 rotors
 // Take inspiration from password cracker lab
 
+
+// Calculates the index of coincidence for a specific value
+float index_of_coincidence(char* text) {
+    int length = strlen(text);
+    int frequency[26];
+    // Initialize frequency to 0
+    for (int i = 0; i < 26; i++) {
+        frequency[i] = 0;
+    }
+
+
+    // Record the frequency of the chracters in the text
+    for (int i = 0; i < length; i++) {
+        int characterex = toupper(text[i]) - 65;
+        frequency[characterex]++;
+    }
+
+    float ioc;
+    for (int i = 0; i < 26; i++) {;
+        ioc += frequency[i] * (frequency[i] - 1);
+    }
+    ioc /= length*(length-1);
+
+    return ioc;
+}
+
 void* worker (void* args) {
     return NULL;
 
 }
 
 
-int main(int argc, char* argv[]) {
+// int main(int argc, char* argv[]) { 
+//     // Take input
+//     char* text = malloc(strlen(argv[1]));
+//     memcpy(text, argv[1], strlen(argv[1])); 
+//     float IOC_rotors[60];
+
+//     // Initializing the IOCs for each rotor setting possible 
+//     for(int i = 0; i < 60; i++){
+//         IOC_rotors[i] = 0;
+//     }
+
+//     // Initialize a random rotor settings
+//     settings_t* setting = malloc(sizeof(settings_t));
+//     for (int i = 0; i < 3; i++) {
+//         setting->rotor_order[i] = 2 - i;
+//         setting->rotor_offset[i] = 0;
+//     }
+
+//     // iterate through each permutation of rotors
+//     int count = 0;
+//     float max1 = 0;
+//     float max2 = 0;
+//     float max3 = 0;
+//     float max4 = 0;
+//     float max5 = 0;
+//     for (int i = 0; i < 5; i++) {
+//         for (int j = 0; j < 5; j++) {
+//             if (j == i) {
+//                 continue;
+//             }        
+//             for (int k = 0; k < 5; k++) {
+//                 if (k == j || k == i) {
+//                     continue;
+//                 }
+
+//                 setting->rotor_order[0] = i;
+//                 setting->rotor_order[1] = j;
+//                 setting->rotor_order[2] = k;
+
+//                 // Encrypt the text
+//                 char* cipher_text = decrypt(text, setting);
+
+//                 // Calculate the index of coincidence
+//                 float ioc = index_of_coincidence(cipher_text);
+
+//                 // printf("%d) IOC: %f\n", count, ioc);
+//                 count++;
+//                 if(max1 < ioc){
+//                     max1 = ioc;
+//                 }
+                
+//             }   
+//         }
+//     }
+//     printf("Best IOC = %f\n", max1);
+    
 
 
-    return 0;
-}
+//     // Free allocated memory
+//     free(setting);
+//     free(text);
+
+//     return 0;
+// }
--- enigma.c
+++ enigma.c
@@ -104,9 +104,67 @@ char* encrypt(char* text, settings_t* setting) {
     return text;
 }
 
+/**
+ * decrypts each character of a given string using thr provided initial settings
+ * Stores the decrypted text back into `text`. 
+ * `text` must be dynamically allocated memory
+ */
+char* decrypt(char* text, settings_t* setting) {
+
+    // Get length of message
+    int text_length = strlen(text);
+
+    int* rotor_order = setting->rotor_order; 
+    int* rotor_offset = setting->rotor_offset;
+
+    // Encrypt each character
+    char current_char;
+    for (int c = 0; c < text_length; c++) {
+        current_char = toupper(text[c]);
+
+
+        //Chose to ignore reflector because it dosen't make sense without a plugboard
+        // Pass back through each rotor (fastest to slowest)
+        for (int r = 0; r >= 2; r++) {
+            
+            // Pass through rotor
+            char* current_rotor = ROTORS[rotor_order[r]];
+
+            // Account for starting offset
+            current_char += rotor_offset[r];
+
+            // Pass character through the rotor
+            current_char = map(current_char, current_rotor);
+
+        }
+
+        // Increment the first rotor
+        rotor_offset[0]++;
+        if (rotor_offset[0] >= 26) {
+            rotor_offset[0] = 0;
+            rotor_offset[1]++;
+        }
+        // If full rotation increment the second rotor
+        if (rotor_offset[1] >= 26) {
+            rotor_offset[1] = 0;
+            rotor_offset[2]++;
+        }
+
+        // If full roation then reset third rotor
+        rotor_offset[2] %= 26;
+
+
+        // Store back into 
+        text[c] = current_char;
+    }
+
+    // Return encrypted text
+    return text;
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 2) {
-        perror("Incorrect number of args");
+        perror("Usage: ./enigma [plain_text]");
         exit(1);
     }
 
@@ -114,7 +172,7 @@ int main(int argc, char* argv[]) {
     char* text = malloc(strlen(argv[1]));
     memcpy(text, argv[1], strlen(argv[1])); 
 
-    // Define rotor settings
+    // Define default rotor settings
     settings_t* setting = malloc(sizeof(settings_t));
     for (int i = 0; i < 3; i++) {
         setting->rotor_order[i] = 2 - i;
@@ -125,6 +183,10 @@ int main(int argc, char* argv[]) {
     char* cipher_text = encrypt(text, setting);
 
     printf("%s\n", cipher_text);
+
+    char* cipher_dec = decrypt(cipher_text, setting);
+
+    printf("%s\n", cipher_dec);
 
     free(setting);
     free(text);
